9d79139e61da90b8e9f60638df21f15f8ca7f8b1
Kevin Bevan <iambevan@gmail.com>
2021-07-31 21:56:30 +0100

    convert to typescript

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 80,
+	semi: false,
+	singleQuote: false,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	arrowParens: "avoid",
+};
